bcf00f8daed5bcfbed4d2a61a347aee969c480d9
Agnes-olofsson <abolofss@syr.edu>
2023-03-23 18:19:27 -0400

    started color picker

--- pages/index.js
+++ pages/index.js
@@ -2,45 +2,39 @@ import Button from '../components/Button';
 import Header from '../components/Header'
 import Layout from '../components/Layout'
 import Image from 'next/image';
+import Swatch from '../components/Swatch';
 
-import { getAllVehicles } from '../lib/api';
+import { getAllVehicles, getAllColors } from '../lib/api';
 
 export async function getStaticProps() {
   // Get external data from the file system, API, DB, etc.
   const data = getAllVehicles();
+  const colors = getAllColors();
 
   // The value of the `props` key will be
   //  passed to the `Home` component
   return {
     props:{
-     data
+     data,
+     colors
     }
   }
 }
 
-const Homepage = ({data}) => {
+const Homepage = ({data, colors}) => {
   console.log({data});
   return <Layout>
   <h1>Homepage</h1>
-  <Button 
-  label="Register Now" 
-  type="primary"
-  /> 
-
-  {data.map((car) => {
-    const { model, slug } = car;
-    return <article>
-      <Image 
-      src={`/vehicles/${slug}/medium.webp`}
-      alt={`${model} car`}
-      width={350}
-      height={185}
-      />
-      <h2>{model}</h2>
-    
-    </article>
-  })}
-
+  <h2>Color Picker</h2>
+  <div>
+    large image goes here
+  </div>
+  <ul>
+    {colors.map((color) => {
+      return <Swatch color={color} />
+    })}
+  </ul>
+  <h3>Color name</h3>
   </Layout>
 }
 
