fa6863a4baa6e9fc59552a08d0f24b22a2dc3634
Agnes-olofsson <abolofss@syr.edu>
2023-04-18 18:19:21 -0400

    configured dynamic route

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,40 @@
-import Button from '../components/Button';
-import CarColorPicker from '../components/CarColorPicker';
-import Header from '../components/Header'
+
 import Layout from '../components/Layout'
-import Image from 'next/image';
-import LocationPicker from '../components/LocationPicker'
-import Showcase from '../components/Showcase'
-import SectionCTA from '../components/SectionCTA'
 
 
-import { getAllVehicles, getAllColors } from '../lib/api';
+
+import {
+   //getAllVehicles, 
+   //getAllColors 
+  } from '../lib/api';
 
 export async function getStaticProps() {
   // Get external data from the file system, API, DB, etc.
-  const data = getAllVehicles();
-  const colors = getAllColors();
+  //const data = getAllVehicles();
+  //const colors = getAllColors();
 
   // The value of the `props` key will be
   //  passed to the `Home` component
   return {
     props:{
-     data,
-     colors
+    // data,
+     //colors
     }
   }
 }
 
-const Homepage = ({data, colors}) => {
+const Homepage = ({
+  //data, 
+  //colors
+}) => {
   //console.log({data});
   return <Layout>
-    <Showcase />
-    <LocationPicker />
-  <CarColorPicker colors={colors} />
-  <SectionCTA />
+    {
+  //<Showcase />
+  // <LocationPicker />
+  //  <CarColorPicker colors={colors} /> 
+  //<SectionCTA />
+    }
   </Layout>
 }
 
